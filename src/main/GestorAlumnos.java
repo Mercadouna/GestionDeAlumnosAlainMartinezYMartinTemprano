d5a06fd4877c95d7a5b5133ef446308db3c7a4f0
Mercadouna <20alain.martinez05@gmail.com>
2025-01-23 12:34:49 +0100

    main

---
+++ src/main/GestorAlumnos.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import clases.Alumno;
+
+public class GestorAlumnos {
+    private List<Alumno> alumnos = new ArrayList<>();
+
+    public void añadirAlumno(Alumno alumno) {
+        alumnos.add(alumno);
+        System.out.println("Alumno añadido: " + alumno);
+    }
+
+    public List<Alumno> getAlumnos() {
+        return alumnos;
+    }
+}
+
---
+++ src/main/Menu.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Menu {
+
+	public void mostrarMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
